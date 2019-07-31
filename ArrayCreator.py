05dd5d22b81f675279512608c4146771a36465b4
imakiro <imakiro@users.noreply.github.com>
2019-07-31 22:58:32 +0200

    Adding the main script python File

---
+++ ArrayCreator.py
@@ -0,0 +1,98 @@
+from PIL import Image
+
+ASCII_CHARS = ['0','1','1','1','2','2','2','3','3','4','4']
+ASCII_CHARS.reverse()
+'''
+method resize():
+    - takes as parameters the image, and the final width
+    - resizes the image into the final width while maintaining aspect ratio
+'''
+def resize(image, new_width=200):
+    (old_width, old_height) = image.size
+    aspect_ratio = float(old_height)/float(old_width)
+    new_height = int(aspect_ratio * new_width)
+    new_dim = (new_width, new_height)
+    new_image = image.resize(new_dim)
+    return new_image
+'''
+method grayscalify():
+    - takes an image as a parameter
+    - returns the grayscale version of image
+'''
+def grayscalify(image):
+    return image.convert('L')
+
+'''
+method modify():
+    - replaces every pixel with a character whose intensity is similar
+'''
+def modify(image, buckets=25):
+    initial_pixels = list(image.getdata())
+    new_pixels = [ASCII_CHARS[pixel_value//buckets] for pixel_value in initial_pixels]
+    return ''.join(new_pixels)
+
+'''
+method do():
+    - does all the work by calling all the above functions
+'''
+def do(image, new_width=200):
+    image = resize(image)
+    image = grayscalify(image)
+
+    pixels = modify(image)
+    len_pixels = len(pixels)
+
+    # Construct the image from the character list
+    new_image = [pixels[index:index+new_width] for index in range(0, len_pixels, new_width)]
+
+    return '\n'.join(new_image)
+
+'''
+method runner():
+    - takes as parameter the image path and runs the above code
+    - handles exceptions as well
+    - provides alternative output options
+'''
+def runner(path):
+    image = None
+    try:
+        image = Image.open(path)
+    except Exception:
+        print("Unable to find image in",path)
+        #print(e)
+        return
+    image = do(image)
+
+    # To print on console
+    # print(image)
+    listeuh = image.split('\n')
+    # print(listeuh)
+    newlist = []
+    for l in listeuh:
+        gneh = [int(i) for i in list(l)]
+        newlist.append(gneh)
+
+    print(newlist)
+
+    # Else, to write into a file
+    # Note: This text file will be created by default under
+    #       the same directory as this python file,
+    #       NOT in the directory from where the image is pulled.
+    f = open('img.txt','w')
+    f.write(image)
+    f.close()
+
+'''
+method main():
+    - reads input from console
+    - profit
+'''
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+    if sys.argv[1].startswith('http://') or sys.argv[1].startswith('https://'):
+        urllib.request.urlretrieve(sys.argv[1], "asciify.jpg")
+        path = "asciify.jpg"
+    else:
+        path = sys.argv[1]
+    runner(path)
